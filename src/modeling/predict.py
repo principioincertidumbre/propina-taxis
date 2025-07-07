cdebf2d12be7db16f351f483f70810544ffbc43f
Sofía Vits Contreras <s.vitsc@udd.cl>
2025-07-07 19:13:03 -0400

    Crea función model_evaluate

--- src/modeling/predict.py
+++ src/modeling/predict.py
@@ -2,9 +2,14 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score
 import joblib
 
-def model_predict(df, features, target_col):
+def model_predict(df, features):
     loaded_rfc = joblib.load("random_forest.joblib")
     preds_test = loaded_rfc.predict_proba(df[features])
     preds_test_labels = [p[1] for p in preds_test.round()]
-    print(f'F1: {f1_score(df[target_col], preds_test_labels)}')
-    return
+    return preds_test_labels
+
+def model_evaluate(df, target_col, preds_test_labels):
+    F1 = f1_score(df[target_col], preds_test_labels)
+    return F1
+
+
