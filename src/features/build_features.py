0f3966917e5513d6d73c920d9fcc3b5b00eb344b
Sofía Vits Contreras <s.vitsc@udd.cl>
2025-07-07 12:58:21 -0400

    Corrige indentación

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -7,12 +7,10 @@ def build_features(df):
     "passenger_count",
     'trip_distance',
     'trip_time',
-    'trip_speed'
-     ]
-     categorical_feat = [
-     "PULocationID",
-     "DOLocationID",
-     "RatecodeID",
-     ]
-     features = numeric_feat + categorical_feat
-     return features
+    'trip_speed']
+    categorical_feat = [
+    "PULocationID",
+    "DOLocationID",
+    "RatecodeID"]
+    features = numeric_feat + categorical_feat
+    return features
