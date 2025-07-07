08ddc7754fd3209436f0573fb6c3aceff9e2fa30
Sofía Vits Contreras <s.vitsc@udd.cl>
2025-07-07 19:15:39 -0400

    Ajusta filas entrenamiento modelo
    Aumenta a 600000 las filas utilizadas para entrenar el modelo

--- src/modeling/train.py
+++ src/modeling/train.py
@@ -2,7 +2,7 @@ from sklearn.ensemble import RandomForestClassifier
 import joblib
 
 def train_model(df, features, target_col):
-    df_train = df.head(100000)
+    df_train = df.head(600000)
     rfc = RandomForestClassifier(n_estimators=100, max_depth=10)
     rfc.fit(df_train[features], df_train[target_col])
     joblib.dump(rfc, "random_forest.joblib")
