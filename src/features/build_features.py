6aee05556db7cd8ae4599aea0666fb403d9a0334
Sofía Vits Contreras <s.vitsc@udd.cl>
2025-07-07 12:25:51 -0400

    Define features modelo propina taxis

---
+++ src/features/build_features.py
@@ -0,0 +1,17 @@
+numeric_feat = [
+    "pickup_weekday",
+    "pickup_hour",
+    'work_hours',
+    "pickup_minute",
+    "passenger_count",
+    'trip_distance',
+    'trip_time',
+    'trip_speed'
+]
+categorical_feat = [
+    "PULocationID",
+    "DOLocationID",
+    "RatecodeID",
+]
+features = numeric_feat + categorical_feat
+EPS = 1e-7
