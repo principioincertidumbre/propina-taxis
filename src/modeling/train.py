9f2a036c5f1ce4fd4793581b5f841c60f625bdcc
Sofía Vits Contreras <s.vitsc@udd.cl>
2025-07-08 11:21:09 -0400

    Actualiza ubicación para model dump

--- src/modeling/train.py
+++ src/modeling/train.py
@@ -5,7 +5,7 @@ def train_model(df, features, target_col):
     df_train = df.head(600000)
     rfc = RandomForestClassifier(n_estimators=100, max_depth=10)
     rfc.fit(df_train[features], df_train[target_col])
-    joblib.dump(rfc, "random_forest.joblib")
+    joblib.dump(rfc, "models/random_forest.joblib")
     return
 
 
