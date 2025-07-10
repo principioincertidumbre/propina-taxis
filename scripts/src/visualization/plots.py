fc84175f7c6f62c6b5ea70f22982563c38afd287
Sofía Vits Contreras <s.vitsc@udd.cl>
2025-07-10 14:15:49 -0400

    Agrega etiquetas ejes x,y

--- scripts/src/visualization/plots.py
+++ scripts/src/visualization/plots.py
@@ -1,14 +1,10 @@
-import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def dataframe(list1,list2,list3):
-    df = pd.DataFrame(list(zip(list1, list2, list3)),
-         columns =['Mes', 'Cantidad ejemplos', 'F1-score'])
-    return df
-
-def lineplot(df,x,y, label):
+def lineplot(df,x,y,xlabel, ylabel, label):
     sns.lineplot(data=df, x=x, y=y)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.title(label, loc='center')
     plt.show()
     return
