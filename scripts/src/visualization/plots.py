2c75a30a03b138ddfb87e55db2c654a0b16691c9
Sofía Vits Contreras <s.vitsc@udd.cl>
2025-07-10 12:18:50 -0400

    Agrega función lineplot

--- scripts/src/visualization/plots.py
+++ scripts/src/visualization/plots.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn
+import seaborn as sns
 
 def dataframe(list1,list2,list3):
     df = pd.DataFrame(list(zip(list1, list2, list3)),
-               columns =['Mes', 'Cantidad ejemplos', 'F1-score'])
+         columns =['Mes', 'Cantidad ejemplos', 'F1-score'])
     return df
+
+def lineplot(df,x,y, label):
+    sns.lineplot(data=df, x=x, y=y)
+    plt.title(label, loc='center')
+    plt.show()
+    return
