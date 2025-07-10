03efab8f1b90c657a32ef5edb924088d86c837ff
Sofía Vits Contreras <s.vitsc@udd.cl>
2025-07-10 14:55:07 -0400

    Ajusta notación eje y
    Evita que use notación científica en eje y

--- scripts/src/visualization/plots.py
+++ scripts/src/visualization/plots.py
@@ -5,6 +5,8 @@ def lineplot(df,x,y,xlabel, ylabel, label):
     sns.lineplot(data=df, x=x, y=y)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
+    plt.yticks(rotation=90)
+    plt.ticklabel_format(style='plain', axis='y') # evita que valores sean formateados en notación científica
     plt.title(label, loc='center')
     plt.show()
     return
