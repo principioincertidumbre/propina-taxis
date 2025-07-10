b8bc772eda321f8ca65eabd79fba06d21b0f6aa3
Sofía Vits Contreras <s.vitsc@udd.cl>
2025-07-10 11:36:52 -0400

    Aumenta filas entrenamiento
    Aumento hasta alcanzar aproximadamente un 66% de las filas del dataset de enero 2020

--- scripts/src/modeling/train.py
+++ scripts/src/modeling/train.py
@@ -2,7 +2,7 @@ from sklearn.ensemble import RandomForestClassifier
 import joblib
 
 def train_model(df, features, target_col):
-    df_train = df.head(600000)
+    df_train = df.head(42120000) # representa aproximadamente un 66% del dataset de enero 2020
     rfc = RandomForestClassifier(n_estimators=100, max_depth=10)
     rfc.fit(df_train[features], df_train[target_col])
     joblib.dump(rfc, "models/random_forest.joblib")
