6a519ce8bbbf8f3f738225cccee182f0fe2a0470
Sofía Vits Contreras <s.vitsc@udd.cl>
2025-07-11 12:45:23 -0400

    Agrega comentarios

--- scripts/src/features/build_features.py
+++ scripts/src/features/build_features.py
@@ -1,16 +1,12 @@
+'''Función para definir atributos numéricos y categóricos'''
+
 def build_features():
-    numeric_feat = [
-    "pickup_weekday",
-    "pickup_hour",
-    'work_hours',
-    "pickup_minute",
-    "passenger_count",
-    'trip_distance',
-    'trip_time',
-    'trip_speed']
+    numeric_feat = ["pickup_weekday",
+    "pickup_hour",'work_hours',
+    "pickup_minute","passenger_count",
+    'trip_distance','trip_time','trip_speed']
     categorical_feat = [
-    "PULocationID",
-    "DOLocationID",
+    "PULocationID","DOLocationID",
     "RatecodeID"]
     features = numeric_feat + categorical_feat
     return features
