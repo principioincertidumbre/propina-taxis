268f749642fd42d3cb6ead99ce1eff65fd331af6
Sofía Vits Contreras <s.vitsc@udd.cl>
2025-07-11 12:46:24 -0400

    Agrega comentarios

--- scripts/src/modeling/train.py
+++ scripts/src/modeling/train.py
@@ -1,11 +1,12 @@
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 
+'''Función para entrenar modelo'''
 def train_model(df, features, target_col):
-    df_train = df.head(42120000) # representa aproximadamente un 66% del dataset de enero 2020
+    df_train = df.head(42120000) # Representa aproximadamente un 66% del dataset de enero 2020
     rfc = RandomForestClassifier(n_estimators=100, max_depth=10)
     rfc.fit(df_train[features], df_train[target_col])
-    joblib.dump(rfc, "models/random_forest.joblib")
+    joblib.dump(rfc, "models/random_forest.joblib") # Guarda modelo entrenado
     return
 
 
